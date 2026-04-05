f3beb91edc2b0ab695b762db57c6496726820c9e
Davide Bianchi <davidebianchi08@gmail.com>
2026-04-05 17:19:00 +0200

    Add automatic redirect to login on token expiration

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: 'https://crm-project-api.onrender.com/api',
 });
 
-// Intercettore: aggiunge il token a ogni richiesta
+// Intercettore richieste: aggiunge il token a ogni richiesta
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
@@ -12,5 +12,18 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
+
+// Intercettore risposte: gestisce token scaduto/invalido
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
